e63e0e1ad3e02e4ede479ad017f6a92f2f664a18
mygame-url <mygame@users.noreply.github.com>
2026-09-24 11:18:48 +0800

    update url

--- url.js
+++ url.js
@@ -1,1 +1,1 @@
-window.__MJ_URL__ = "https://gaotai-mahjing-v1ehkdkh.edgeone.cool?eo_token=7fe3cb1d95d399c886a7af2bd1ba4888&eo_time=1790215872";
+window.__MJ_URL__ = "https://gaotai-mahjing-v1ehkdkh.edgeone.cool?eo_token=c925c43a4315c002f747b5b865e1df04&eo_time=1790219909";
