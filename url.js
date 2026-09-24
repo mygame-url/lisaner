aa634b2190f7942c68dc945c9a466ce69fd6c9c3
relay <relay@local>
2026-09-24 20:04:43 +0800

    update url

--- url.js
+++ url.js
@@ -1,1 +1,1 @@
-window.__MJ_URL__ = "https://gaotai-mahjing-v1ehkdkh.edgeone.cool?eo_token=e2e492dbb2fb8161ab3ce9365b10a595&eo_time=1790250871";
+window.__MJ_URL__ = "https://gaotai-mahjing-v1ehkdkh.edgeone.cool?eo_token=df4a1d8f7c8c5b87f44f064704d00e91&eo_time=1790251465";
