7596cde3ff5a423b301929a8504140705c679c08
mo-yunfeng <mo@local>
2026-09-25 23:00:56 +0800

    update url v3.87

--- url.js
+++ url.js
@@ -1,1 +1,1 @@
-window.__MJ_URL__ = "https://gaotai-mahjing-v1ehkdkh.edgeone.cool?eo_token=d53ebc165f288c14dddacbf9803c5676&eo_time=1790317222";
+window.__MJ_URL__ = "https://gaotai-mahjing-v1ehkdkh.edgeone.cool?eo_token=627a967ad79b76c417e749f0b8eb8f3e&eo_time=1790348376";
