27b725a9dad147c017011d982ae20ae38271f734
mo-yunfeng <mo@local>
2026-09-26 14:04:12 +0800

    update url

--- url.js
+++ url.js
@@ -1,1 +1,1 @@
-window.__MJ_URL__ = "https://gaotai-mahjing-v1ehkdkh.edgeone.cool?eo_token=627a967ad79b76c417e749f0b8eb8f3e&eo_time=1790348376";
+window.__MJ_URL__ = "https://gaotai-mahjing-v1ehkdkh.edgeone.cool?eo_token=1592a5d465b8d8c54db3d30a303136df&eo_time=1790402635";
